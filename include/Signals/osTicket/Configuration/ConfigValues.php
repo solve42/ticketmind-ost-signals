57650e8e94606e562d82b02ed3e3cc8ccc54cc9f
eumas42 <eugen.massini@solve42.de>
2025-09-02 14:08:42 +0200

    license header (gpl-2 only)

--- include/Signals/osTicket/Configuration/ConfigValues.php
+++ include/Signals/osTicket/Configuration/ConfigValues.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * TicketMind Signals Plugin — TicketMind API Client
+ * Copyright (C) 2025  Solve42 GmbH
+ * Author: Eugen Massini <info@solve42.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
 
 namespace TicketMind\Plugin\Signals\osTicket\Configuration;
 
